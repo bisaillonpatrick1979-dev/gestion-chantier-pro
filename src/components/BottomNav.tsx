6c48915c3bc3f9a0dee156d31493dcb3b960c8bf
Hailite <bisaillonpatrick1979@gmail.com>
2026-05-15 15:42:02 -0600

    Update BottomNav.tsx

--- src/components/BottomNav.tsx
+++ src/components/BottomNav.tsx
@@ -1,1 +1,9 @@
+'use client';
 
+export default function BottomNav() {
+  return (
+    <nav className="fixed bottom-0 left-0 right-0 bg-white border-t border-gray-200 p-4">
+      {/* Navigation items */}
+    </nav>
+  );
+}
